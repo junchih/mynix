d2db3ded18e81eb6f6a21a4147af4216b8870272
Jack J. Mao <junchih.1893@gmail.com>
2023-03-06 13:57:44 +0800

    Guard sshd

--- lbhost/conf.sshd.nix
+++ lbhost/conf.sshd.nix
@@ -3,10 +3,24 @@
 with builtins;
 {
   services.openssh = {
+
     enable = trace "Enable OpenSSH" true;
     useDns = false;
+    banner = null;
+
     permitRootLogin = "no";
     passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+
+    extraConfig = ''
+      Protocol 2
+      Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+      LoginGraceTime 30
+      MaxAuthTries 2
+      MaxSessions 5
+      MaxStartups 5:30:30
+    '';
+
     ports = [
       22
       44307
